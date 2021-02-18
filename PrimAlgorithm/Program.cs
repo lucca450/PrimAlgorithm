afc3e138012b35a866a7443fe8eee8cf53dd6216
bellevin <bellevin@uqtr.ca>
2021-02-18 12:36:04 -0500

    Hello world program

---
+++ PrimAlgorithm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
